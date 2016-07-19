120f59b8849951982c914c077cf9f0ee4fa30446
Sergio Saldarriaga <sergio.saldarriaga88@gmail.com>
2016-07-19 12:52:58 -0400

    adding initial files

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	$("#mon0").click(function(){
+		var move = $("#mon0").detach();
+		move.appendTo(".chosenChar");
+		var rest = $("#mon1,#mon2,#mon3").detach();
+		rest.appendTo(".enemies");
+	});
+	$("#mon1").click(function(){
+		var move = $("#mon1").detach();
+		move.appendTo(".chosenChar");
+		var rest = $("#mon0,#mon2,#mon3").detach();
+		rest.appendTo(".enemies");
+	});
+	$("#mon2").click(function(){
+		var move = $("#mon2").detach();
+		move.appendTo(".chosenChar");
+		var rest = $("#mon0,#mon1,#mon3").detach();
+		rest.appendTo(".enemies");
+	});
+	$("#mon3").click(function(){
+		var move = $("#mon3").detach();
+		move.appendTo(".chosenChar");
+		var rest = $("#mon0,#mon1,#mon2").detach();
+		rest.appendTo(".enemies");
+	});
+})
